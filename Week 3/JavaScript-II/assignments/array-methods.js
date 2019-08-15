889863fb56e3f5dee327dd23343bc3a38a25eec8
occludedarp <cmrnmr@gmail.com>
2019-08-14 21:09:22 -0400

    wrote first line of mouseover event

---
+++ Week 3/JavaScript-II/assignments/array-methods.js
@@ -0,0 +1,502 @@
+$_$wp(1);
+const runners = ($_$w(1, 0), [
+    {
+        'id': 1,
+        'first_name': 'Charmain',
+        'last_name': 'Seiler',
+        'email': 'cseiler0@wired.com',
+        'shirt_size': '2XL',
+        'company_name': 'Divanoodle',
+        'donation': 75
+    },
+    {
+        'id': 2,
+        'first_name': 'Whitaker',
+        'last_name': 'Ierland',
+        'email': 'wierland1@angelfire.com',
+        'shirt_size': '2XL',
+        'company_name': 'Wordtune',
+        'donation': 148
+    },
+    {
+        'id': 3,
+        'first_name': 'Julieta',
+        'last_name': 'McCloid',
+        'email': 'jmccloid2@yahoo.com',
+        'shirt_size': 'S',
+        'company_name': 'Riffpedia',
+        'donation': 171
+    },
+    {
+        'id': 4,
+        'first_name': 'Martynne',
+        'last_name': 'Paye',
+        'email': 'mpaye3@sciencedaily.com',
+        'shirt_size': 'XL',
+        'company_name': 'Wordware',
+        'donation': 288
+    },
+    {
+        'id': 5,
+        'first_name': 'Gussy',
+        'last_name': 'Raraty',
+        'email': 'graraty4@ucoz.ru',
+        'shirt_size': 'L',
+        'company_name': 'Oozz',
+        'donation': 291
+    },
+    {
+        'id': 6,
+        'first_name': 'Yule',
+        'last_name': 'Tommasetti',
+        'email': 'ytommasetti5@state.gov',
+        'shirt_size': 'S',
+        'company_name': 'Yodo',
+        'donation': 27
+    },
+    {
+        'id': 7,
+        'first_name': 'Kathie',
+        'last_name': 'Majury',
+        'email': 'kmajury6@guardian.co.uk',
+        'shirt_size': '3XL',
+        'company_name': 'Zoomcast',
+        'donation': 261
+    },
+    {
+        'id': 8,
+        'first_name': 'Tanner',
+        'last_name': 'Branton',
+        'email': 'tbranton7@tmall.com',
+        'shirt_size': '2XL',
+        'company_name': 'Realmix',
+        'donation': 28
+    },
+    {
+        'id': 9,
+        'first_name': 'Sarina',
+        'last_name': 'Lasham',
+        'email': 'slasham8@toplist.cz',
+        'shirt_size': 'XL',
+        'company_name': 'Gigashots',
+        'donation': 110
+    },
+    {
+        'id': 10,
+        'first_name': 'Bertie',
+        'last_name': 'Lonergan',
+        'email': 'blonergan9@issuu.com',
+        'shirt_size': '3XL',
+        'company_name': 'Skinte',
+        'donation': 62
+    },
+    {
+        'id': 11,
+        'first_name': 'Trevor',
+        'last_name': 'Studd',
+        'email': 'tstudda@networkadvertising.org',
+        'shirt_size': 'S',
+        'company_name': 'Cogidoo',
+        'donation': 76
+    },
+    {
+        'id': 12,
+        'first_name': 'Malachi',
+        'last_name': 'Okeshott',
+        'email': 'mokeshottb@chron.com',
+        'shirt_size': 'M',
+        'company_name': 'DabZ',
+        'donation': 91
+    },
+    {
+        'id': 13,
+        'first_name': 'Berget',
+        'last_name': 'Logsdail',
+        'email': 'blogsdailc@wix.com',
+        'shirt_size': 'M',
+        'company_name': 'Mymm',
+        'donation': 9
+    },
+    {
+        'id': 14,
+        'first_name': 'Loise',
+        'last_name': 'Rivlin',
+        'email': 'lrivlind@behance.net',
+        'shirt_size': '2XL',
+        'company_name': 'Linktype',
+        'donation': 204
+    },
+    {
+        'id': 15,
+        'first_name': 'Christan',
+        'last_name': 'Kendall',
+        'email': 'ckendalle@example.com',
+        'shirt_size': 'XS',
+        'company_name': 'Skinix',
+        'donation': 252
+    },
+    {
+        'id': 16,
+        'first_name': 'Kayla',
+        'last_name': 'Whitwam',
+        'email': 'kwhitwamf@deliciousdays.com',
+        'shirt_size': '2XL',
+        'company_name': 'Oyope',
+        'donation': 147
+    },
+    {
+        'id': 17,
+        'first_name': 'Heddie',
+        'last_name': 'Heningam',
+        'email': 'hheningamg@tripadvisor.com',
+        'shirt_size': 'L',
+        'company_name': 'Skinix',
+        'donation': 172
+    },
+    {
+        'id': 18,
+        'first_name': 'Mace',
+        'last_name': 'Ballinger',
+        'email': 'mballingerh@a8.net',
+        'shirt_size': 'S',
+        'company_name': 'Nlounge',
+        'donation': 266
+    },
+    {
+        'id': 19,
+        'first_name': 'Nola',
+        'last_name': 'Abberley',
+        'email': 'nabberleyi@jalbum.net',
+        'shirt_size': 'XL',
+        'company_name': 'Photospace',
+        'donation': 148
+    },
+    {
+        'id': 20,
+        'first_name': 'Nadine',
+        'last_name': 'Tresler',
+        'email': 'ntreslerj@marketwatch.com',
+        'shirt_size': '3XL',
+        'company_name': 'Wikido',
+        'donation': 292
+    },
+    {
+        'id': 21,
+        'first_name': 'Ulrikaumeko',
+        'last_name': 'Vuittet',
+        'email': 'uvuittetk@gov.uk',
+        'shirt_size': 'S',
+        'company_name': 'Skinte',
+        'donation': 102
+    },
+    {
+        'id': 22,
+        'first_name': 'Saunder',
+        'last_name': 'Spennock',
+        'email': 'sspennockl@icq.com',
+        'shirt_size': '3XL',
+        'company_name': 'Kwimbee',
+        'donation': 213
+    },
+    {
+        'id': 23,
+        'first_name': 'Carmel',
+        'last_name': 'Woffinden',
+        'email': 'cwoffindenm@wikispaces.com',
+        'shirt_size': 'S',
+        'company_name': 'Rooxo',
+        'donation': 137
+    },
+    {
+        'id': 24,
+        'first_name': 'Marielle',
+        'last_name': 'Kimmel',
+        'email': 'mkimmeln@jimdo.com',
+        'shirt_size': 'M',
+        'company_name': 'Livetube',
+        'donation': 96
+    },
+    {
+        'id': 25,
+        'first_name': 'Brucie',
+        'last_name': 'Burris',
+        'email': 'bburriso@slate.com',
+        'shirt_size': '2XL',
+        'company_name': 'Wordtune',
+        'donation': 128
+    },
+    {
+        'id': 26,
+        'first_name': 'Juan',
+        'last_name': 'Berzon',
+        'email': 'jberzonp@soup.io',
+        'shirt_size': '3XL',
+        'company_name': 'Einti',
+        'donation': 234
+    },
+    {
+        'id': 27,
+        'first_name': 'Sacha',
+        'last_name': 'Olsen',
+        'email': 'solsenq@reverbnation.com',
+        'shirt_size': '2XL',
+        'company_name': 'Viva',
+        'donation': 190
+    },
+    {
+        'id': 28,
+        'first_name': 'Jamey',
+        'last_name': 'O\'Nolan',
+        'email': 'jonolanr@samsung.com',
+        'shirt_size': 'XL',
+        'company_name': 'Skinix',
+        'donation': 31
+    },
+    {
+        'id': 29,
+        'first_name': 'Morrie',
+        'last_name': 'Rainard',
+        'email': 'mrainards@yale.edu',
+        'shirt_size': 'XS',
+        'company_name': 'Podcat',
+        'donation': 52
+    },
+    {
+        'id': 30,
+        'first_name': 'Fidel',
+        'last_name': 'Roskelly',
+        'email': 'froskellyt@ibm.com',
+        'shirt_size': 'XS',
+        'company_name': 'Avavee',
+        'donation': 5
+    },
+    {
+        'id': 31,
+        'first_name': 'Toni',
+        'last_name': 'MacSweeney',
+        'email': 'tmacsweeneyu@parallels.com',
+        'shirt_size': 'M',
+        'company_name': 'Jaloo',
+        'donation': 82
+    },
+    {
+        'id': 32,
+        'first_name': 'Jessey',
+        'last_name': 'Walhedd',
+        'email': 'jwalheddv@slashdot.org',
+        'shirt_size': 'L',
+        'company_name': 'Trilia',
+        'donation': 5
+    },
+    {
+        'id': 33,
+        'first_name': 'Karola',
+        'last_name': 'Piper',
+        'email': 'kpiperw@ucsd.edu',
+        'shirt_size': '3XL',
+        'company_name': 'Yombu',
+        'donation': 110
+    },
+    {
+        'id': 34,
+        'first_name': 'Marley',
+        'last_name': 'Mitchenson',
+        'email': 'mmitchensonx@webeden.co.uk',
+        'shirt_size': 'M',
+        'company_name': 'Zoonoodle',
+        'donation': 97
+    },
+    {
+        'id': 35,
+        'first_name': 'Marrilee',
+        'last_name': 'Thrasher',
+        'email': 'mthrashery@opensource.org',
+        'shirt_size': 'XL',
+        'company_name': 'Bluejam',
+        'donation': 17
+    },
+    {
+        'id': 36,
+        'first_name': 'Tye',
+        'last_name': 'Manie',
+        'email': 'tmaniez@netscape.com',
+        'shirt_size': 'L',
+        'company_name': 'Kanoodle',
+        'donation': 30
+    },
+    {
+        'id': 37,
+        'first_name': 'Charleen',
+        'last_name': 'Sheering',
+        'email': 'csheering10@mit.edu',
+        'shirt_size': '3XL',
+        'company_name': 'Jatri',
+        'donation': 262
+    },
+    {
+        'id': 38,
+        'first_name': 'Valma',
+        'last_name': 'Eynaud',
+        'email': 'veynaud11@archive.org',
+        'shirt_size': 'XS',
+        'company_name': 'Jaxbean',
+        'donation': 212
+    },
+    {
+        'id': 39,
+        'first_name': 'Dollie',
+        'last_name': 'McDarmid',
+        'email': 'dmcdarmid12@tinyurl.com',
+        'shirt_size': 'S',
+        'company_name': 'Kayveo',
+        'donation': 74
+    },
+    {
+        'id': 40,
+        'first_name': 'Minna',
+        'last_name': 'Hymas',
+        'email': 'mhymas13@cornell.edu',
+        'shirt_size': 'XS',
+        'company_name': 'Vimbo',
+        'donation': 101
+    },
+    {
+        'id': 41,
+        'first_name': 'Jsandye',
+        'last_name': 'Frend',
+        'email': 'jfrend14@ca.gov',
+        'shirt_size': 'XS',
+        'company_name': 'Latz',
+        'donation': 156
+    },
+    {
+        'id': 42,
+        'first_name': 'Yevette',
+        'last_name': 'Hacket',
+        'email': 'yhacket15@wp.com',
+        'shirt_size': 'XL',
+        'company_name': 'Lazzy',
+        'donation': 291
+    },
+    {
+        'id': 43,
+        'first_name': 'Hank',
+        'last_name': 'Zebedee',
+        'email': 'hzebedee16@ezinearticles.com',
+        'shirt_size': 'L',
+        'company_name': 'Gigashots',
+        'donation': 241
+    },
+    {
+        'id': 44,
+        'first_name': 'Jodie',
+        'last_name': 'Stawell',
+        'email': 'jstawell17@yale.edu',
+        'shirt_size': 'S',
+        'company_name': 'Jaxspan',
+        'donation': 262
+    },
+    {
+        'id': 45,
+        'first_name': 'Falito',
+        'last_name': 'Karsh',
+        'email': 'fkarsh18@pcworld.com',
+        'shirt_size': 'S',
+        'company_name': 'Mycat',
+        'donation': 239
+    },
+    {
+        'id': 46,
+        'first_name': 'Reginauld',
+        'last_name': 'Purselowe',
+        'email': 'rpurselowe19@thetimes.co.uk',
+        'shirt_size': 'L',
+        'company_name': 'Jabbersphere',
+        'donation': 11
+    },
+    {
+        'id': 47,
+        'first_name': 'Vida',
+        'last_name': 'Tydd',
+        'email': 'vtydd1a@dropbox.com',
+        'shirt_size': 'S',
+        'company_name': 'Quaxo',
+        'donation': 55
+    },
+    {
+        'id': 48,
+        'first_name': 'Anderea',
+        'last_name': 'MacGiolla Pheadair',
+        'email': 'amacgiollapheadair1b@xing.com',
+        'shirt_size': '2XL',
+        'company_name': 'Kwimbee',
+        'donation': 214
+    },
+    {
+        'id': 49,
+        'first_name': 'Bel',
+        'last_name': 'Alway',
+        'email': 'balway1c@ow.ly',
+        'shirt_size': 'S',
+        'company_name': 'Voolia',
+        'donation': 107
+    },
+    {
+        'id': 50,
+        'first_name': 'Shell',
+        'last_name': 'Baine',
+        'email': 'sbaine1d@intel.com',
+        'shirt_size': 'M',
+        'company_name': 'Gabtype',
+        'donation': 171
+    }
+]);
+let fullName = ($_$w(1, 1), []);
+function bibNames(runner) {
+    $_$wf(1);
+    $_$w(1, 2), fullName.push(`${ runner.first_name } ${ runner.last_name }`);
+}
+$_$w(1, 3), runners.forEach(bibNames);
+$_$w(1, 4), $_$tracer.log(fullName, 'fullName', 1, 4);
+let allCaps = ($_$w(1, 5), []);
+function toCaps(runner) {
+    $_$wf(1);
+    $_$w(1, 6), runner.first_name.toUpperCase();
+}
+$_$w(1, 7), $_$tracer.log(runners.map(toCaps), 'runners.map(toCaps)', 1, 7);
+let largeShirts = ($_$w(1, 8), []);
+function wearsLarge(runner) {
+    $_$wf(1);
+    if ($_$w(1, 9), runner.shirt_size === 'L') {
+        $_$w(1, 10), largeShirts.push(runner);
+    }
+}
+$_$w(1, 11), runners.filter(wearsLarge);
+$_$w(1, 12), $_$tracer.log(largeShirts, 'largeShirts', 1, 12);
+let ticketPriceTotal = ($_$w(1, 13), runners.reduce(function (accumulator, currentItem) {
+    $_$wf(1);
+    return $_$w(1, 14), accumulator + currentItem.donation;
+}, 0));
+$_$w(1, 15), $_$tracer.log(ticketPriceTotal, 'ticketPriceTotal', 1, 15);
+let emailList = ($_$w(1, 16), []);
+function getEmail(runner) {
+    $_$wf(1);
+    $_$w(1, 17), emailList.push(runner.email);
+}
+$_$w(1, 18), runners.forEach(getEmail);
+$_$w(1, 19), $_$tracer.log(emailList, 'emailList', 1, 19);
+let runnerCompanies = ($_$w(1, 20), []);
+function companyRep(runner) {
+    $_$wf(1);
+    $_$w(1, 21), runnerCompanies.push(`${ runner.first_name } is proud to be representing ${ runner.company_name }!`);
+}
+$_$w(1, 22), runners.forEach(companyRep);
+$_$w(1, 23), $_$tracer.log(runnerCompanies, 'runnerCompanies', 1, 23);
+let runnersInitials = ($_$w(1, 24), []);
+function makeInitials(runner) {
+    $_$wf(1);
+    $_$w(1, 25), runnersInitials.push(`${ runner.id }: ${ runner.first_name.charAt(0) }${ runner.last_name.charAt(0) }`);
+}
+$_$w(1, 26), runners.map(makeInitials);
+$_$w(1, 27), $_$tracer.log(runnersInitials, 'runnersInitials', 1, 27);
+$_$wpe(1);
--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+//* Assignment: Create Unique Event Listeners
+
+//* Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use;
+
+	//* `mouseover`  'Funbus' h1 bounces up and down 
+	//* `keydown` p blurs when keydown is used
+	//* `wheel`  h2 slightly drifts up when wheel is used
+	//* `drag//*//*drop`
+	//* `load` 
+	//* `focus` 
+	//* `resize` 
+	//* `scroll` 
+	//* `select` 
+	//* `dblclick`
+
+//* Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+
+// * Nest two similar events somewhere in the site and prevent the event propagation properly
+
+// * Stop the navigation items from refreshing the page by using `preventDefault()`
+
+const logoHeading = document.querySelector('.nav-container .logo-heading');
+
+logoHeading.addEventListener('mouseover', (e) => {
+	console.log('it\'s working')
+})
